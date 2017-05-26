2db46723743fc021d1751aad014fed9707628976
jijunwei <jijunwei@witpos.com>
2017-05-26 15:17:05 +0800

    创建有赞转发模块并设置依赖于biz

---
+++ hytdemo-biz/src/main/java/com/pekon/hytdemo/App.java
@@ -0,0 +1,14 @@
+package com.pekon.hytdemo;
+
+/**
+ * Created by elvisji on 2017/5/25.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public static void hell() {
+        System.out.println("hello world");
+    }
+}
---
+++ youzan-push-forward-web/src/main/java/com/pekon/hytdemo/Test.java
@@ -0,0 +1,10 @@
+package com.pekon.hytdemo;
+
+/**
+ * Created by elvisji on 2017/5/25.
+ */
+public class Test {
+    public static void main(String[] args) {
+        App app = new App();
+    }
+}
